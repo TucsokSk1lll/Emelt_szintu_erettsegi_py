6303aab4f79905b5e7b2eb7a612cb25d77126854
Tucsok <jgyorgy008@gmail.com>
2024-03-18 21:03:54 +0100

    Összes 70 éves infó érettségit összeállító tanár szopja le magát 2x

--- 2012_oktober/szinkep.py
+++ 2012_oktober/szinkep.py
@@ -1,4 +1,4 @@
-x = open(file="2012_oktober\kep.txt", mode="r")
+x = open(file="2012_oktober\\kep.txt", mode="r+")
 x = x.read()
 x = x.split("\n")
 x.pop()
@@ -7,12 +7,13 @@ for i in range(len(x)):
 
 lst = []
 lst2 = []
+index = 0
 for i in range(50):
-    index = 0
     for j in range(50):
         lst2.append(x[index])
         index += 1
     lst.append(lst2)
+    lst2 = []
 
 for i in range(50):
     for j in range(50):
@@ -27,6 +28,8 @@ for i in range(50):
     for j in range(50):
         if lst[i][j] == rgb:
             lol = True
+            
+print(lol)
 
 print('3. feladat')
 
@@ -34,14 +37,38 @@ sor = 0
 oszlop = 0
 
 for i in range(50):
-    for j in range(50):
-        if i == 34 and lst[34][7] == lst[i][j]:
-            sor += 1
+    if lst[34][7] == lst[34][i]:
+        sor += 1
 
 for i in range(50):
     for j in range(50):
         if j == 7 and lst[34][7] == lst[i][j]:
             oszlop += 1
 
-print(sor, oszlop, lst[34][7])
+print(sor, oszlop,)
+
+print('4. feladat')
+
+voros = 0
+kek = 0
+zold = 0
+
+for i in range(50):
+    for j in range(50):
+        if lst[i][j] == [255,0,0]:
+            voros += 1
+        elif lst[i][j] == [0,255,0]:
+            zold += 1
+        elif lst[i][j] == [0,0,255]:
+            kek += 1
+
+if voros > zold and voros > kek:
+    print('Vörös')
+elif zold > voros and zold > kek:
+    print('Zöld')
+else:
+    print('Kék')
+    
+print('5. feladat')
+
             
