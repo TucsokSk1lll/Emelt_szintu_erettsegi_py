77e53eba27ed1c4b7cad322324683709acf0cc1e
Tucsok <jgyorgy008@gmail.com>
2024-03-27 09:29:25 +0100

    halál

--- 2014_majus/loves.py
+++ 2014_majus/loves.py
@@ -36,9 +36,108 @@ for i in range(len(lst)):
     lovesek = len(lst[i])
     lovesek_szama.append(lovesek)
 
-for i in lovesek_szama:
-    if i == max(lovesek_szama):
+for i in range(len(lovesek_szama)):
+    if lovesek_szama[i] == max(lovesek_szama):
         print(i+1)
 
 
-print(lovesek_szama)
+def loertek(sor):
+    aktpont = 20
+    ertek = 0
+    for i in range (len(sor)):
+        if aktpont > 0 and sor[i] == '0':
+            aktpont -= 1
+        else: 
+            ertek += aktpont
+    return ertek
+
+
+print('5. feladat')
+
+sorszam = 1
+
+talalt_lovesek = []
+
+for i in range(len(lst[sorszam-1])):
+    if lst[sorszam-1][i] == 1:
+        talalt_lovesek.append(i+1)
+
+print(talalt_lovesek)
+
+talalt_lovesek_szama = 0
+
+for i in lst[sorszam-1]:
+    if i == 1:
+        talalt_lovesek_szama += 1
+
+print(talalt_lovesek_szama)
+
+talalt_lovesek_sorozatai = []
+
+sorozat = 0
+for i in lst[sorszam-1]:
+    if i == 1:
+        sorozat += 1
+        talalt_lovesek_sorozatai.append(sorozat)
+    else:
+        sorozat = 0
+        talalt_lovesek_sorozatai.append(sorozat)
+        
+
+print(max(talalt_lovesek_sorozatai))
+
+sztringxd = ''
+
+for i in lst[sorszam-1]:
+    if i == 1:
+        sztringxd += '1'
+    else:
+        sztringxd += '0'
+
+print(loertek(sztringxd))
+
+sorrend = []
+
+for i in range(len(lst)):
+    versenyzo = []
+
+    versenyzo.append(0)
+
+    versenyzo.append(0)
+
+    sztringlol = ''
+    for j in lst[i]:
+        if j == 1:
+            sztringlol += '1'
+        else:
+            sztringlol += '0'
+
+    versenyzo.append(loertek(sztringlol))
+    sorrend.append(versenyzo)
+
+#print(sorrend)
+sorrend_masolat = []
+for i in range(len(sorrend)):
+    sorrend_masolat.append(sorrend[i].copy())
+for i in range(len(sorrend)):
+    sorrend_masolat[i][1] = i+1
+#print(sorrend_masolat)
+
+sorrend.sort(reverse=True)
+#print(sorrend)
+
+for i in range(len(sorrend)):
+    sorrend[i][0] = i+1
+
+print(sorrend)
+print('\n')
+print(sorrend_masolat)
+print('\n')
+
+for i in range(len(sorrend)):
+    for j in range(len(sorrend)):
+        if sorrend[i][2] == sorrend_masolat[j][2]:
+            sorrend[i][1] = sorrend_masolat[j][1]
+            break
+
+print(sorrend)
